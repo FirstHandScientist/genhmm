de606edfb4e26081d45827fc41f2dcdebc7b915c
Antoine Honoré <honore@kth.se>
2019-04-18 17:45:04 +0200

    Simple flow with Toy data.

--- practice/realnvp.py
+++ practice/realnvp.py
@@ -14,45 +14,7 @@ from torch.nn.parameter import Parameter
 
 from sklearn import cluster, datasets, mixture
 from sklearn.preprocessing import StandardScaler
-
-class RealNVP(nn.Module):
-    def __init__(self, nets, nett, mask, prior):
-        super(RealNVP, self).__init__()
-
-        self.prior = prior
-        self.mask = nn.Parameter(mask, requires_grad=False)
-        self.t = torch.nn.ModuleList([nett() for _ in range(len(masks))])
-        self.s = torch.nn.ModuleList([nets() for _ in range(len(masks))])
-
-    def g(self, z):
-        x = z
-        for i in range(len(self.t)):
-            x_ = x * self.mask[i]
-            s = self.s[i](x_) * (1 - self.mask[i])
-            t = self.t[i](x_) * (1 - self.mask[i])
-            x = x_ + (1 - self.mask[i]) * (x * torch.exp(s) + t)
-        return x
-
-    def f(self, x):
-        log_det_J, z = x.new_zeros(x.shape[0]), x
-        for i in reversed(range(len(self.t))):
-            z_ = self.mask[i] * z
-            s = self.s[i](z_) * (1 - self.mask[i])
-            t = self.t[i](z_) * (1 - self.mask[i])
-            z = (1 - self.mask[i]) * (z - t) * torch.exp(-s) + z_
-            log_det_J -= s.sum(dim=1)
-        return z, log_det_J
-
-    def log_prob(self, x):
-        z, logp = self.f(x)
-        return self.prior.log_prob(z) + logp
-
-    def sample(self, batchSize):
-        z = self.prior.sample((batchSize, 1))
-        logp = self.prior.log_prob(z)
-        x = self.g(z)
-        return x
-
+from src.realnvp import RealNVP
 
 if __name__ == "__main__":
     H = 28
@@ -65,6 +27,7 @@ if __name__ == "__main__":
     nets = lambda: nn.Sequential(nn.Linear(d, H), nn.LeakyReLU(), nn.Linear(H, H), nn.LeakyReLU(), nn.Linear(H, d),
                                  nn.Tanh())
     nett = lambda: nn.Sequential(nn.Linear(d, H), nn.LeakyReLU(), nn.Linear(H, H), nn.LeakyReLU(), nn.Linear(H, d))
+
     masks = torch.from_numpy(np.array([[0, 1], [1, 0]] * nchain).astype(np.float32))
     prior = distributions.MultivariateNormal(torch.zeros(d), torch.eye(d))
     flow = RealNVP(nets, nett, masks, prior)
