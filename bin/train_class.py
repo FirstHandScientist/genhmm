9f68397b2989397794558e9b383a71029d69a023
Antoine Honore <honore@kth.se>
2019-07-04 14:38:49 +0000

    Move pad data and TheDataset to utils, load models on CPU by default for compatibility.

--- bin/train_class.py
+++ bin/train_class.py
@@ -4,51 +4,16 @@ sys.path.append("..")
 
 from parse import parse
 import pickle as pkl
-from src.genHMM import  GenHMM, save_model, load_model
-import numpy as np
+from src.genHMM import GenHMM, save_model, load_model
+from src.utils import pad_data, TheDataset
 import torch
-from torch.utils.data import Dataset, DataLoader
-from torch import distributions
-
-from datetime import datetime as dt
-
-class TheDataset(Dataset):
-    """Silly wrapper for DataLoader input."""
-    def __init__(self, xtrain, lengths, device='cpu'):
-        self.data = [torch.FloatTensor(x).to(device) for x in xtrain]
-        self.lengths = lengths
-        max_len_ = self.data[0].shape[0]
-        self.mask = [torch.cat( (torch.ones(l, dtype=torch.uint8), \
-                                torch.zeros( max_len_ - l, dtype=torch.uint8))).to(device) \
-                     for l in self.lengths]
-        self.len=len(self.data)
-        
-
-    def __getitem__(self, index):
-        # mask = torch.cat( (torch.ones(self.lengths[index], dtype=torch.uint8), \
-        #                    torch.zeros(self.data[index].shape[0] \
-        #                                - self.lengths[index], dtype=torch.uint8)) )
-        return (self.data[index], self.mask[index])
-
-    def __len__(self):
-        return self.len
-
-
-def pad_data(x,length):
-    """ Add zeros at the end of all sequences in to get sequences of lengths `length`
-    Input:  x : list, all of sequences of variable length to pad
-            length : integer, common target length of sequences.
-    output: list,  all input sequnces zero-padded.
-    """
-
-    d = x[0].shape[1]
-    return [np.concatenate((xx,np.zeros((length - xx.shape[0] + 1,d)))) for xx in x]
-
+from torch.utils.data import DataLoader
+import json
 
 
 if __name__ == "__main__":
-    usage = "python bin/train_class.py data/train13.pkl models/epoch1_class1.mdlc"
-    if len(sys.argv) != 3 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    usage = "python bin/train_class.py data/train13.pkl models/epoch1_class1.mdlc param.json"
+    if len(sys.argv) < 3 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
         print(usage)
         sys.exit(1)
 
@@ -56,57 +21,45 @@ if __name__ == "__main__":
     train_inputfile = sys.argv[1]
     out_mdl = sys.argv[2]
 
+    # Test for a third parameter
+    try:
+        param_file = sys.argv[3]
+    except IndexError:
+        param_file = "default.json"
+
+
     epoch_str, iclass_str = parse('epoch{}_class{}.mdlc',os.path.basename(out_mdl))
     train_class_inputfile = train_inputfile.replace(".pkl", "_{}.pkl".format(iclass_str))
 
     #  Load data
     
     xtrain = pkl.load(open(train_class_inputfile, "rb"))
-    xtrain=xtrain[:100]
+    xtrain = xtrain[:100]
     # Get the length of all the sequences
     l = [x.shape[0] for x in xtrain]
 
     #  Load or create model
     if epoch_str == '1':
-
-        #  Create model, to
-        options = dict(n_states=3, n_prob_components=2,
-                       em_skip=30, device='cpu', lr=1e-4, log_dir="results")
+        with open(param_file) as f_in:
+            options = json.load(f_in)
 
         mdl = GenHMM(**options)
 
     else:
         # Load previous model
-
         mdl = load_model(out_mdl.replace("epoch" + epoch_str, "epoch" + str(int(epoch_str)-1)))
-    
+
     mdl.iepoch = epoch_str
     mdl.iclass = iclass_str
-    
+
+
     mdl.device = 'cpu'
     if torch.cuda.is_available():
         device = torch.device('cuda')
         mdl.device = device
     
     print("epoch:{}\tclass:{}\tPush model to {}...".format(epoch_str,iclass_str, mdl.device), file=sys.stdout)
-    for s in range(mdl.n_states):
-        for k in range(mdl.n_prob_components):
-            # push new networks to device
-            mdl.networks[s,k].to(mdl.device)
-            p = mdl.networks[s,k].prior
-            mdl.networks[s,k].prior = type(p)(p.loc.to(mdl.device),
-                                              p.covariance_matrix.to(mdl.device))
-            
-            # push the old networks to device
-            mdl.old_networks[s,k].to(mdl.device)
-            p = mdl.old_networks[s,k].prior
-            mdl.old_networks[s,k].prior = type(p)(p.loc.to(mdl.device),
-                                              p.covariance_matrix.to(mdl.device))
-            
-    mdl.startprob_ = mdl.startprob_.to(mdl.device)
-    mdl.transmat_ = mdl.transmat_.to(mdl.device)
-    mdl.logPIk_s = mdl.pi.log().to(mdl.device)
-    
+    mdl.pushto(mdl.device)   
     
     
     # zero pad data for batch training
@@ -120,13 +73,18 @@ if __name__ == "__main__":
     
     # em_skip determines the number of back-props before an EM step is performed
     mdl.em_skip = 5
-    
+
     # TODO: pass lr as a param
     mdl.lr = 1e-3
 
+    # add number of training data in model
+    mdl.number_training_data = len(xtrain)
+
     for iiter in range(niter):
         mdl.fit(traindata)
-        
+
+    # Push back to cpu for compatibility when GPU unavailable.
+    mdl.pushto('cpu')
     save_model(mdl, fname=out_mdl)
     sys.exit(0)
 
