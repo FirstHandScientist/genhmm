b1c76b55dd6d7debdfb65da886b93a4549396660
Antoine Honoré <honore@kth.se>
2019-07-23 19:01:40 +0200

    Don t parse actually. and passes the class model to carry all needed info (mdl folder, epoch iclass)

--- bin/compute_accuracy_class.py
+++ bin/compute_accuracy_class.py
@@ -2,20 +2,20 @@ import os
 import sys
 from parse import parse
 import pickle as pkl
-from gm_hmm.src.utils import append_class,accuracy_fun
+from gm_hmm.src.utils import append_class, accuracy_fun, divide, parse_
 from functools import partial
 
 if __name__ == "__main__":
-    usage = "bin/compute_accuracy_class.py epoch2_class1 13feats/data/train.13.pkl 13feats/data/test.13.pkl 13feats/models"
-    if len(sys.argv) != 5 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+    usage = "bin/compute_accuracy_class.py 13feats/models/epoch2_class1.mdlc 13feats/data/train.13.pkl 13feats/data/test.13.pkl"
+    if len(sys.argv) != 4 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
         print(usage, file=sys.stderr)
         sys.exit(1)
 
-    epoch, iclass = parse("epoch{:d}_class{:d}", sys.argv[1])
+    models_dir, epoch, iclass = parse("{}/epoch{:d}_class{:d}.mdlc", sys.argv[1])
 
     training_data_file = sys.argv[2]
     testing_data_file = sys.argv[3]
-    models_dir = sys.argv[4]
+
     mdl_file = os.path.join(models_dir, "epoch{}.mdl".format(epoch))
 
     # Load Model
@@ -27,9 +27,10 @@ if __name__ == "__main__":
 
     # Builds an array of string containing the train and test data sets for each class
     # size: nclass x 2 (train, test)
-    data_files = [append_class(training_data_file, iclass + 1), append_class(testing_data_file, iclass + 1)]
-    f = partial(accuracy_fun, mdl=mdl)
-
+    data_files = [append_class(training_data_file, iclass), append_class(testing_data_file, iclass)]
+    #f = lambda x: divide(parse_(accuracy_fun(x, mdl=mdl)))
+    f = lambda x: accuracy_fun(x, mdl=mdl)
     results = list(map(f, data_files))
 
-    print("epoch: {} class: {} train: {} test: {}".format(epoch, iclass + 1, results[0], results[1]))
+    print("epoch: {} class: {} accc train: {} test: {}".format(epoch, iclass, results[0], results[1]), file=sys.stdout)
+    sys.exit(0)
