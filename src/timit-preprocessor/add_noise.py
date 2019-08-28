ae464e1888add6db355bf56895db3ad63d345dd7
Antoine Honoré <honore@kth.se>
2019-08-28 14:41:46 +0200

    change the option for easier integration with make

--- src/timit-preprocessor/add_noise.py
+++ src/timit-preprocessor/add_noise.py
@@ -3,6 +3,8 @@ from multiprocessing.dummy import Pool
 from scipy.io import wavfile
 import argparse
 from glob import glob
+from parse import parse
+
 import os, sys
 import numpy as np
 
@@ -13,9 +15,9 @@ def gen_noise(n, type, sigma, noise_folder="../../data/NoiseDB/NoiseX_16kHz"):
     if type == "white":
         noise_filename = os.path.join(noise_folder, "{}_16kHz.wav".format(type))
         _, loaded_noise = wavfile.read(noise_filename)
+
         try:
             assert(n < loaded_noise.shape[0])
-
         except AssertionError as e:
             print("Noise file: {} is too short.".format(noise_filename), file=sys.stderr)
 
@@ -30,45 +32,49 @@ def gen_noise(n, type, sigma, noise_folder="../../data/NoiseDB/NoiseX_16kHz"):
     return raw_noise * sigma
 
 
-def new_filename(file, type, snr):
+def new_filename(file, ntype, snr):
     """Append noise tyep and power at the end of wav filename."""
-    return file.replace(".WAV", ".WAV.{}.{}dB".format(type, snr))
+    return file.replace(".WAV", ".WAV.{}.{}dB".format(ntype, snr))
 
 
-def corrupt_data(s, type, snr):
+def corrupt_data(s, ntype, snr):
     """Corrupt a signal with a particular noise."""
     s_std = np.std(s)
     n_std = 10 ** (- snr / 10) * s_std
-    n = gen_noise(s.shape[0], type, n_std)
+    n = gen_noise(s.shape[0], ntype, n_std)
     sn = (s + n).astype(s.dtype)
     return sn
 
 
-def corrupt_wav(file, type=None, snr=None):
+def corrupt_wav(file, ntype=None, snr=None):
     """Corrupt a wav file with noise and write to a new file."""
     rate, s = wavfile.read(file)
-    sn = corrupt_data(s, type, snr)
-    wavfile.write(new_filename(file, type, snr), rate, sn)
+    sn = corrupt_data(s, ntype, snr)
+    wavfile.write(new_filename(file, ntype, snr), rate, sn)
     return 0
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Add a particular noise type to WAV files.")
 
-    parser.add_argument('-i', metavar="<Input folder>", type=str)
-    parser.add_argument('-snr', metavar="<Signal to Noise Ratio (dB)>", type=int)
-    parser.add_argument('-type', metavar="<Noise type>", type=str)
+    parser.add_argument('-timit', metavar="<Timit location>", type=str)
+    parser.add_argument('-opt', metavar="<Signal to Noise Ratio (dB)>", type=str)
     parser.add_argument('-j', metavar="<Number of jobs (default: numcpu)>",
                         type=int, default=os.cpu_count())
 
     args = parser.parse_args()
+    try:
+        dset, ntype, snr = parse("{}.{}.{:d}dB", args.opt)
+    except TypeError as e:
+        print("No noise to be added with option: {}.\nExit.".format(args.opt),file=sys.stderr)
+        sys.exit(0)
 
-    print(args.i, args.snr, args.type)
 
-    wavs = glob(os.path.join(args.i, "**" , "*.WAV"), recursive=True)
-    f = partial(corrupt_wav, type=args.type, snr=args.snr)
-    f(wavs[1])
-    with Pool(args.j) as pool:
-        pool.map(f, wavs)
+    if dset == "test":
+        wavs = glob(os.path.join(args.timit, "TEST", "**" , "*.WAV"), recursive=True)
+        f = partial(corrupt_wav, ntype=ntype, snr=snr)
+
+        with Pool(args.j) as pool:
+            pool.map(f, wavs)
 
     sys.exit(0)
